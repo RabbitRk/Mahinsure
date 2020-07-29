833802dcdd791cb669e9af50c229aaa79c5512f0
RkRabbit <emailtork09@gmail.com>
2020-07-29 18:34:01 +0530

    Final local Commit

--- app/src/main/java/com/rabbitt/mahinsure/Config.java
+++ app/src/main/java/com/rabbitt/mahinsure/Config.java
@@ -2,10 +2,18 @@ package com.rabbitt.mahinsure;
 
 public class Config {
     public static final String TOKEN_PREF = "TOKEN_PREF";
-    public static final String GET_REF_DATA = "http://192.168.43.231/mahinsure/get_ref_no";
-    public static final String LOGIN = "http://192.168.43.231/mahinsure/login";
-    public static final String GET_DATA = "http://192.168.43.231/mahinsure/get_data";
-    public static final String TOKEN_UPDATE = "http://192.168.43.231/mahinsure/token_update";
-    static final String UPLOAD = "http://192.168.43.231/mahinsure/upload";
-    static final String DEMO = "http://192.168.43.231/mahinsure/demo";
+
+//    public static final String GET_REF_DATA = "http://192.168.43.67/mahinsure/get_ref_no";
+//    public static final String LOGIN = "http://192.168.43.67/mahinsure/login";
+//    public static final String GET_DATA = "http://192.168.43.67/mahinsure/get_data";
+//    public static final String TOKEN_UPDATE = "http://192.168.43.67/mahinsure/token_update";
+//    static final String UPLOAD = "http://192.168.43.67/mahinsure/upload";
+//    static final String DEMO = "http://192.168.43.67/mahinsure/demo";
+
+    public static final String GET_REF_DATA = "http://192.168.10.15/bracket/get_ref_no";
+    public static final String LOGIN = "http://192.168.10.15/bracket/login";
+    public static final String GET_DATA = "http://192.168.10.15/bracket/get_data";
+    public static final String TOKEN_UPDATE = "http://192.168.10.15/bracket/token_update";
+    static final String UPLOAD = "http://192.168.10.15/bracket/upload";
+    static final String DEMO = "http://192.168.10.15/bracket/demo";
 }
